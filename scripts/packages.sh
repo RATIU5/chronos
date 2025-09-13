73f35824a254d8f8ffca25a586f6960eba2a4206
RATIU5 <jcmemmott20@outlook.com>
2025-09-12 18:31:42 -0600

    edit package installer script

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -11,18 +11,22 @@ install-local-pkgbuild() {
 	x popd
 }
 
-machine=$(gum_choose "Beelink SER8" --prompt "Select your device:" --limit 1)
+echo "Select your device: "
+machine=$(gum_choose "Beelink SER8" --limit 1)
 
+local metapkgs=()
 case $machine in
 	"Beelink SER8")
-		$CHRONOS_CONFIRM_EVERY_STEP && 
+		metapkgs=(./packages/chronos-{beelink-ser8})
+		;;
 	*) echo "Unsupported device selected. Exiting."; exit 1 ;;
 esac
 
-metapkgs=(./packages/chronos-{beelink-ser8})
 
 for i in "${metapkgs[@]}"; do
 	metainstallflags="--needed"
 	$CHRONOS_CONFIRM_EVERY_STEP && showfun install-local-pkgbuild || metainstallflags="$metainstallflags --noconfirm"
 	v install-local-pkgbuild "$i" "$metainstallflags"
 done
+
+echo "All selected packages installed."
