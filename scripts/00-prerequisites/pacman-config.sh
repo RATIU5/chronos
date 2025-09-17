04650cebaf7d63a0022bb21fe5a28e9cc34cab0c
RATIU5 <jcmemmott20@outlook.com>
2025-09-17 08:52:43 -0600

    add gpg key for cachyos repo

--- scripts/00-prerequisites/pacman-config.sh
+++ scripts/00-prerequisites/pacman-config.sh
@@ -16,6 +16,8 @@ main() {
     temp_dir=$(mktemp -d)
     trap 'rm -rf -- "$temp_dir"' EXIT
 		
+		# Import the GPG key for the CachyOS repository
+		execute gpg --keyserver keyserver.ubuntu.com --recv-keys F3B607488DB35A47
     execute curl -L https://mirror.cachyos.org/cachyos-repo.tar.xz -o "$temp_dir/cachyos-repo.tar.xz"
     execute bash -c "tar xvf $temp_dir/cachyos-repo.tar.xz -C $temp_dir && sudo $temp_dir/cachyos-repo/cachyos-repo.sh"
     gum_style --foreground="#50fa7b" "✓ CachyOS repository added."
