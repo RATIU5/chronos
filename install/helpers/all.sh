66090cd98fa68a01655fd1dc5f6d00b110521a95
RATIU5 <jcmemmott20@outlook.com>
2025-09-29 08:53:03 -0600

    fix variables

--- install/helpers/all.sh
+++ install/helpers/all.sh
@@ -1,4 +1,4 @@
 source $CHRONOS_INSTALL/helpers/chroot.sh
-source $OMARCHY_INSTALL/helpers/presentation.sh
-source $OMARCHY_INSTALL/helpers/errors.sh
-source $OMARCHY_INSTALL/helpers/logging.sh
+source $CHRONOS_INSTALL/helpers/presentation.sh
+source $CHRONOS_INSTALL/helpers/errors.sh
+source $CHRONOS_INSTALL/helpers/logging.sh
