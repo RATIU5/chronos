76022c09a0606a90d51f1f2ea87714a785179351
RATIU5 <jcmemmott20@outlook.com>
2025-09-29 09:31:09 -0600

    add pacman conf

--- install/preflight/pacman.sh
+++ install/preflight/pacman.sh
@@ -2,6 +2,10 @@ if [[ -n ${CHRONOS_ONLINE_INSTALL:-} ]]; then
 	# Install build tools
 	sudo pacman -S --needed --noconfirm base-devel
 
+  # Copy default pacman configuration files
+  sudo cp -f ~/.local/share/chronos/default/pacman/pacman.conf /etc/pacman.conf
+  sudo cp -f ~/.local/share/chronos/default/pacman/mirrorlist /etc/pacman.d/mirrorlist
+
 	# Configure pacman
 	sudo pacman -Syu --noconfirm
 fi
