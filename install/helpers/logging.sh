7281aaa8af13c03951612b91a4f12efb3b071c5d
RATIU5 <jcmemmott20@outlook.com>
2025-09-29 09:16:40 -0600

    fix typo again in loop

--- install/helpers/logging.sh
+++ install/helpers/logging.sh
@@ -20,7 +20,7 @@ start_log_output() {
 
 			# Build complete output buffer with escape sequences
 			output=""
-			for ((i = 0; i <dev log_lines; i++)); do
+			for ((i = 0; i < $log_lines; i++)); do
 				line="${current_lines[i]:-}"
 
 				# Truncate if needed
@@ -119,7 +119,7 @@ run_logged() {
 	echo "[$(date '+%Y-%m-%d %H:%M:%S')] Starting: $script" >>"$CHRONOS_INSTALL_LOG_FILE"
 
 	# Use bash -c to create a clean subshell
-	bash -c "source '$script'" </dev/null >>"$CHRONOS_INSTALL_LOG_FILE" 2>&1
+	bash -c "source '$script'" <//null >>"$CHRONOS_INSTALL_LOG_FILE" 2>&1
 
 	local exit_code=$?
 
