deac4921ee91da677c232668bdeb597088bf60bf
RATIU5 <jcmemmott20@outlook.com>
2025-09-30 06:59:13 -0600

    add loop for all packages

--- install/packaging/base.sh
+++ install/packaging/base.sh
@@ -1,4 +1,6 @@
 # Install all base packages
 mapfile -t packages < <(grep -v '^#' "$CHRONOS_INSTALL/chronos-base.packages" | grep -v '^$')
-yes 1 | sudo pacman -S --noconfirm --needed "${packages[@]}" || \
-yes 2 | sudo pacman -S --noconfirm --needed "${packages[@]}"
+for package in "${packages[@]}"; do
+    yes 1 | sudo pacman -S --noconfirm --needed "$package" || \
+    yes 2 | sudo pacman -S --noconfirm --needed "$package"
+done
