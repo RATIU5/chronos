e1b5314b7eed50fcece9b3e2586bbf50e2a8d768
RATIU5 <jcmemmott20@outlook.com>
2025-09-30 06:53:43 -0600

    add yes to resolve package conflicting repositories

--- install/packaging/base.sh
+++ install/packaging/base.sh
@@ -1,3 +1,3 @@
 # Install all base packages
 mapfile -t packages < <(grep -v '^#' "$CHRONOS_INSTALL/chronos-base.packages" | grep -v '^$')
-sudo pacman -S --noconfirm --needed "${packages[@]}"
+yes 1 | sudo pacman -S --noconfirm --needed "${packages[@]}"
