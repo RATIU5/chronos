d42b2db907738f1e3c6aa0056bbcc297f7281839
RATIU5 <jcmemmott20@outlook.com>
2025-09-30 06:57:03 -0600

    add another fallback if 1 is not available

--- install/packaging/base.sh
+++ install/packaging/base.sh
@@ -1,3 +1,4 @@
 # Install all base packages
 mapfile -t packages < <(grep -v '^#' "$CHRONOS_INSTALL/chronos-base.packages" | grep -v '^$')
-yes 1 | sudo pacman -S --noconfirm --needed "${packages[@]}"
+yes 1 | sudo pacman -S --noconfirm --needed "${packages[@]}" || \
+yes 2 | sudo pacman -S --noconfirm --needed "${packages[@]}"
