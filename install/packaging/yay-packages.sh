fb14f82934b37e988220c91019013091409330b6
RATIU5 <jcmemmott20@outlook.com>
2025-10-02 08:49:25 -0600

    fux typo to install yay packages

--- install/packaging/yay-packages.sh
+++ install/packaging/yay-packages.sh
@@ -1,3 +1,3 @@
-# Install all base packages
-mapfile -t packages < <(grep -v '^#' "$CHRONOS_INSTALL/chronos-base.packages" | grep -v '^$')
+# Install all yay packages
+mapfile -t packages < <(grep -v '^#' "$CHRONOS_INSTALL/chronos-yay.packages" | grep -v '^$')
 yes '' | sudo yay -S --noconfirm --needed "${packages[@]}"
