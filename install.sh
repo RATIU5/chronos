16b2da66564929eae4018d0d6b19c008131a50bd
RATIU5 <jcmemmott20@outlook.com>
2025-10-02 07:58:35 -0600

    add online install var

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 export CHRONOS_PATH="$HOME/.local/share/chronos"
 export CHRONOS_INSTALL="$CHRONOS_PATH/install"
 export CHRONOS_INSTALL_LOG_FILE="/var/log/chronos-install.log"
+export CHRONOS_ONLINE_INSTALL="true"
 
 source "$CHRONOS_INSTALL/helpers/all.sh"
 source "$CHRONOS_INSTALL/preflight/all.sh"
