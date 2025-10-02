ac6f63be5925024bb21a7a44cd9cf0d363eeee62
RATIU5 <jcmemmott20@outlook.com>
2025-10-02 07:35:35 -0600

    rm cachy filter

--- install/packaging/cachyos-packages.sh
+++ install/packaging/cachyos-packages.sh
@@ -1,3 +1,3 @@
 # Install base packages from the CachyOS package list
 mapfile -t packages < <(grep -v '^#' "$CHRONOS_INSTALL/chronos-cachyos.packages" | grep -v '^$')
-sudo pacman -S --noconfirm --needed "${packages[@]/#/cachyos/}"
+sudo pacman -S --noconfirm --needed "${packages[@]}"
