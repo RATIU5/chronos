4a509533c0c89177f7bb53c3182273f8fa209638
RATIU5 <jcmemmott20@outlook.com>
2025-10-02 06:46:38 -0600

    split packages to install

--- install/packaging/all.sh
+++ install/packaging/all.sh
@@ -1,2 +1,3 @@
 run_logged $CHRONOS_INSTALL/packaging/cachyos-packages.sh
 run_logged $CHRONOS_INSTALL/packaging/base.sh
+run_logged $CHRONOS_INSTALL/packaging/yay-packages.sh
--- install/packaging/yay-packages.sh
+++ install/packaging/yay-packages.sh
@@ -1,3 +1,3 @@
 # Install all base packages
 mapfile -t packages < <(grep -v '^#' "$CHRONOS_INSTALL/chronos-base.packages" | grep -v '^$')
-sudo pacman -S --noconfirm --needed "${packages[@]}"
+sudo yay -S --noconfirm --needed "${packages[@]}"
