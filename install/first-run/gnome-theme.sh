0ba00db05a952ecc0c79bfec154da5d0d8f22eab
RATIU5 <jcmemmott20@outlook.com>
2025-10-03 07:45:29 -0600

    add config stuff

---
+++ install/first-run/gnome-theme.sh
@@ -0,0 +1,5 @@
+gsettings set org.gnome.desktop.interface gtk-theme "Awaita-dark"
+gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
+gsettings set org.gnome.desktop.wm.preferences icon-theme "Yaru-blue"
+
+sudo gtk-update-icon-cache /usr/share/icons/Yaru
---
+++ install/first-run/welcome.sh
@@ -0,0 +1,2 @@
+notify-send "    Update System" "When you have internet, click to update the system." -u critical
+notify-send "👋  Welcome to Chronos" "You're in for a great computing adventure. Have fun!" -t 30000
