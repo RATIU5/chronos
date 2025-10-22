b0436a60ebb8324ab459e11dc6277ab75e769d8c
RATIU5 <jcmemmott20@outlook.com>
2025-10-22 13:35:44 -0600

    add config sync process

--- install/config/config.sh
+++ install/config/config.sh
@@ -4,3 +4,13 @@ cp -R ~/.local/share/chronos/config/* ~/.config/
 
 # Use default bashrc from Chronos
 cp ~/.local/share/chronos/default/bashrc ~/.bashrc
+
+# Copy bin scripts to PATH
+mkdir -p ~/.local/bin
+cp -R ~/.local/share/chronos/bin/* ~/.local/bin/
+chmod +x ~/.local/bin/chronos-*
+
+# Ensure ~/.local/bin is in PATH
+if [[ ":$PATH:" != *":$HOME/.local/bin:"* ]]; then
+    echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc
+fi
