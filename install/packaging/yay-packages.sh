7dfa1f61c65f53cec012c16677e173c7b32c9fe3
RATIU5 <jcmemmott20@outlook.com>
2025-10-21 19:55:38 -0600

    don't install yay packages as root

--- install/packaging/yay-packages.sh
+++ install/packaging/yay-packages.sh
@@ -1,3 +1,3 @@
 # Install all yay packages
 mapfile -t packages < <(grep -v '^#' "$CHRONOS_INSTALL/chronos-yay.packages" | grep -v '^$')
-yes '' | sudo yay -S --noconfirm --needed "${packages[@]}"
+yes '' | yay -S --noconfirm --needed "${packages[@]}"
