c55db8530ba0dfb2f702200f40f412d0e085c776
RATIU5 <jcmemmott20@outlook.com>
2025-10-23 06:36:35 -0600

    copy new common applications folder

--- install/config/applications.sh
+++ install/config/applications.sh
@@ -1,3 +1,4 @@
 # Copy over Chronos desktop applications
 mkdir -p ~/.local/share/applications
 cp -R ~/.local/share/chronos/applications/hidden/* ~/.local/share/applications/
+cp -R ~/.local/share/chronos/applications/common/* ~/.local/share/applications/
